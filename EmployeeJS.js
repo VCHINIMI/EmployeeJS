f0d51933a250a884723ac17fef275d1da878be20
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-10 11:46:44 +0530

    [Vinay]Added: Check if employee is present or absent

--- EmployeeJS.js
+++ EmployeeJS.js
@@ -1,1 +1,14 @@
 //  Welcome to Employee Wage Calculator Problem In Java Script
+
+//  Check If Employee Is Present or Absent
+{
+    const IS_ABSENT = 0 ; 
+    let empCheck = Math.floor(Math.random() * 10)%2;
+    if(empCheck == IS_ABSENT) {
+        console.log("Employee is ABSENT");    
+        return;
+    }
+    else {
+        console.log("Employee is PRESENT");
+    }
+}
