8e35edd9f94cb7f8d9589559d5d241d5df03cc0e
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-10 14:16:14 +0530

    [Vinay]Added: Calculate wage for 20 days by for loop

--- EmployeeJS.js
+++ EmployeeJS.js
@@ -13,12 +13,12 @@
 }
 
 // Calculate Employee Wage
+const WAGE_PER_HOUR = 20 ; // Declaring as global for using wage calculator function anywhere
 {
     const IS_PART_TIME = 1 ;
     const IS_FULL_TIME = 2 ;
     const PART_TIME_HOURS = 4 ;
     const FULL_TIME_HOURS = 8 ;
-    const WAGE_PER_HOUR = 20 ;
 
     function getWorkingHours(empCheck) {
         switch (empCheck) {
@@ -34,4 +34,16 @@
     let empHrs = getWorkingHours(empCheck);
     let empWage = empHrs * WAGE_PER_HOUR ;
     console.log("Employee Wage = " + empWage);
+}
+
+// Calculate Wage for 20 days
+{
+    let empHrs = 0;
+    const WORKING_DAYS = 20 ;
+    for(let i = 0; i <= WORKING_DAYS; i++) {
+        let empCheck = Math.floor(Math.random() * 10)%3;
+        empHrs = empHrs + getWorkingHours(empCheck);
+    }
+    let empWage = empHrs * WAGE_PER_HOUR ;
+    console.log("Aggregate Wage for 20 days = " + empWage);
 }
